9653f9006b1fe8d224077ef8ef44618aa8bd4f7f
Dima Rymar <dimarymar@gmail.com>
2014-11-05 14:21:38 +0200

    add admin_flag to user and fix active_admin

--- Gemfile
+++ Gemfile
@@ -56,3 +56,4 @@ gem 'acts_as_list'
 
 #Admin UI
 gem 'activeadmin', github: 'activeadmin'
+gem 'meta_search'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141105104027) do
+ActiveRecord::Schema.define(version: 20141105115633) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20141105104027) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "authentication_token"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
