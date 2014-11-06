7900fdd29c83bbbd74ea0859e4204b729845dad2
Dima Rymar <dimarymar@gmail.com>
2014-11-06 13:21:46 +0200

    fix therapist create

--- app/admin/therapist.rb
+++ app/admin/therapist.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register Therapist do
   actions :all, except: [:show ]
 
-  permit_params :first_name, :last_name, :email, :password_digest, :password_salt
+  permit_params :first_name, :last_name, :email, :skype_username, :password, :password_confirmation
 
   index do
     column :first_name
@@ -9,5 +9,17 @@ ActiveAdmin.register Therapist do
     column :email
 
     actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :first_name, label: "Name"
+      f.input :last_name, label: "Surname"
+      f.input :email, label: "e-mail"
+      f.input :skype_username, label: "Skype name"
+      f.input :password, label: "Password"
+      f.input :password_confirmation, label: "Confirm Password"
+    end
+    f.actions
   end
 end
--- app/models/therapist.rb
+++ app/models/therapist.rb
@@ -4,7 +4,4 @@ class Therapist < ActiveRecord::Base
   has_many :relationships
   has_many :clients, through: :relationships
 
-  def password= new_password
-    self.pasword_digest = ::BCrypt::Password.create(new_password, cost: 10).to_s
-  end
 end
